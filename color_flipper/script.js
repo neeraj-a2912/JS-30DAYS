5a8f43667e4bf3c2f70729f91cd4dbd6997947d3
Neeraj Aloney <neeraj.aloney2001@gmail.com>
2023-10-03 21:25:58 +0530

    Color Flipper

---
+++ color_flipper/script.js
@@ -0,0 +1,13 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const flipper = document.getElementById("flipper");
+
+flipper.addEventListener("click", function () {
+  let color = "#";
+  for (i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * hex.length)];
+  }
+  document.body.style.backgroundColor = color;
+  flipper.innerText = color;
+});
+
+

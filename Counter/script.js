60b6b89cb94f71d7ef1ac896fa3b5ff77f47ba1b
Neeraj Aloney <neeraj.aloney2001@gmail.com>
2023-12-27 15:41:18 +0530

    changed the text font

--- Counter/script.js
+++ Counter/script.js
@@ -1,10 +1,6 @@
 let counter = document.getElementById("number");
 let current = 0;
 
-if (current < 0) {
-  document.body.style.backgroundColor = "red";
-}
-
 const increment = document.getElementById("increment");
 increment.addEventListener("click", function () {
   current++;
@@ -22,5 +18,3 @@ reset.addEventListener("click", function () {
   current = 0;
   counter.textContent = current;
 });
-
-

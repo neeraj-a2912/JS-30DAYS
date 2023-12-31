8231c323fdddd42d2145eab48ca73560f11cd402
Neeraj Aloney <neeraj.aloney2001@gmail.com>
2023-12-31 18:39:57 +0530

    Creating div elements manually

--- TODO/script.js
+++ TODO/script.js
@@ -1,10 +1,19 @@
-const inputValue = document.getElementById("task");
-const taskList = document.getElementById("task-list");
+const allTodos = document.getElementById("allTasks");
+const addTodo = document.getElementById("addTodo");
+const todoInput = document.getElementById("newTask");
 
-const addTask = () => {
-  let li = document.createElement("li");
-  console.log(inputValue.value);
-  li.innerHTML = inputValue.value;
-  taskList.appendChild(li);
-  inputValue.value = "";
+const addNewTask = (e) => {
+  e.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("card");
+  todoDiv.classList.add("mb-2");
+  const newTodo = document.createElement("div");
+  newTodo.innerText = todoInput.value;
+  console.log(todoInput.value);
+  newTodo.classList.add("card-body");
+  todoDiv.appendChild(newTodo);
+  allTodos.append(todoDiv);
+  todoInput.value = "";
 };
+
+addTodo.addEventListener("click", addNewTask);
